9fa6ad0b6d59591cd8a6bcbf9ebb813b97d77436
jj <swlgus03@gmail.com>
2022-05-14 20:16:35 +0900

    이렇게 나오는 결과가 맞나? 이거 0513 수업 다시 보고 확인하기!

---
+++ Reveiw/src/r0612/C02Else.java
@@ -0,0 +1,21 @@
+package r0612;
+
+public class C02Else {
+
+	public static void main(String[] args) {
+		// If-Else statement
+		
+		int visitCnt = 2;
+		
+		if(visitCnt < 1) {
+			System.out.println("첫방문");
+			visitCnt++;
+		}
+		
+		else {
+			System.out.println("재방문");
+		}
+
+	}
+
+}
