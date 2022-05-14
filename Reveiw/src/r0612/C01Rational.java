85b7b3fc7ab49b5f46033b0ae616c2fca3f5a0f0
jj <swlgus03@gmail.com>
2022-05-14 18:32:50 +0900

    비교 관계 연산자

---
+++ Reveiw/src/r0612/C01Rational.java
@@ -0,0 +1,22 @@
+package r0612;
+
+public class C01Rational {
+
+	public static void main(String[] args) {
+		//비교관계 연산자
+		int num1 = 10;
+		int num2 = 5;
+		
+		System.out.println(num1 == num1); //==는 같다 , =는 저장
+		System.out.println(num1 == num2);
+		System.out.println(num1 != num2); //!= 같지않다
+		System.out.println(num1 > num2);
+		System.out.println(num1 < num2);
+		System.out.println(num1 >= num2);
+		System.out.println(num1 <= num2); //부등호 순서 잘 맞추기
+		
+
+
+	}
+
+}
